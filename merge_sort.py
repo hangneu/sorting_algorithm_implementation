1f57b600b9ef1b0015a8664cc8437b97dbad38c7
shanghang <shangneu1992@gmail.com>
2017-06-22 14:50:28 -0400

    divider and conquer algorithm implementation

---
+++ merge_sort.py
@@ -0,0 +1,11 @@
+def conquer(left, right):
+	l, r, result = 0, 0, []
+	while(l<len(left) and r < len(right)):
+		[result,l,r] = [result + [right[r]],l,r+1] if left[l] > right[r] else [result + [left[l]],l+1,r]
+	return result+left[l:]+right[r:]
+def divider(data_array):
+	length = len(data_array)
+	if(length==0 or length==1): return data_array
+	left=divider(data_array[:length/2] if length%2==0 else data_array[:length/2+1])
+	right=divider(data_array[length/2:] if length%2==0 else data_array[length/2+1:])
+	return conquer(left,right)
---
+++ readme.md
@@ -0,0 +1,9 @@
+# this is test for readme.md# 
+this is for compare above language
+
+## double check what is ##
+```bash
+# run python file method
+# first input "pythonto get into the python environment"
+python
+```
